00d31e99dea10e4dbe52fb945a48bcc8412c2d80
123 <223>
2024-05-25 02:40:40 +0800

    sdfasf

---
+++ src/main/java/com/example/jenkinstest/demos/web/JenkinsTest.java
@@ -0,0 +1,14 @@
+package com.example.jenkinstest.demos.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsTest {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Hello ";
+    }
+}
